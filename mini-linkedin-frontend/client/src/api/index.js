c94ea207813fca49574934173e801c9077f191d8
Haribabu-g <haribabu.g707@gmail.com>
2025-08-04 02:15:36 +0530

    Configure API URL for production

--- mini-linkedin-frontend/client/src/api/index.js
+++ mini-linkedin-frontend/client/src/api/index.js
@@ -1,18 +1,15 @@
-// src/api/index.js
+
 import axios from 'axios';
 
-// Create an instance of axios
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend server URL
+  baseURL: 'https://mini-linkedin-api-yk6n.onrender.com/', 
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-/*
-  Intercepts every request. If a token exists in local storage, 
-  it adds it to the request's 'x-auth-token' header.
-*/
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
